c8f2bf6b42da11e2729f9a3d7c8b2da669fe5aab
Yash Pandey <yashpandey2727@gmail.com>
2022-11-26 10:27:26 +0530

    routes modified

--- router/routes.js
+++ router/routes.js
@@ -1,7 +1,9 @@
 const route = require("express").Router();
 
 const signup = require('../controller/signup');
+const login = require('../controller/login');
 
 route.post("/signup", signup);
+route.post("/login", login);
 
 module.exports = route;
